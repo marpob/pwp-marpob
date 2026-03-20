2c08e20aa41101e49db55c1bdd3049678cb81059
Martin Poboril <mpoboril@cnm.edu>
2026-03-19 20:43:43 -0600

    Fixed navigation bar

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,7 +4,7 @@ import { useLanguage } from "../context/LanguageContext.tsx";
 export function Navigation() {
     const {lang, toggleLang } = useLanguage();
     return (
-        <Navbar fluid rounded className={'border-6 border-gray-200 bg-gray-100'}>
+        <Navbar fluid rounded className={'border-6 border-gray-200 bg-gray-100 fixed top-0 w-full z-50'}>
             <NavbarBrand href="/">
                 <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">{lang === "en" ? "Martin Poboril" : "Martin Pobořil"}</span>
             </NavbarBrand>
